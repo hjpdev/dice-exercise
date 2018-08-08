36512f67773d2efa5cd8239085d2b516b6db7fbb
Harry <harryjp1995@gmail.com>
2018-08-08 20:40:50 +0100

    Test Updates.

--- Dice/dice_spec.rb
+++ Dice/dice_spec.rb
@@ -11,7 +11,7 @@ describe Dice do
     expect(subject.num_die).to eq 1
   end
 
-  it '#roll should return an integer between 1-6.' do
+  it '#roll should return an integer to roll_results, between 1-6.' do
     expect(subject.roll[0].between?(1, 6) && subject.roll[0].is_a?(Integer)).to eq true
   end
 
@@ -23,5 +23,11 @@ describe Dice do
   it 'Should allow the user to roll numerous dice at the same time.' do
     dice = Dice.new(3)
     expect(dice.roll.length).to eq 3
+  end
+
+  it 'Returns the results from numerous dice rolls' do
+    dice = Dice.new(3)
+    allow(dice).to receive(:roll).and_return([1, 2, 3])
+    expect(dice.roll).to eq [1, 2, 3]
   end
 end
